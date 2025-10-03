9422a9115eb06be3aa8adfd4ca91d368d95f638d
Talib Baig <132820490+TalibBaig7@users.noreply.github.com>
2025-10-04 04:34:36 +0530

    Fix: added video file inside public/videos

--- src/Components/Home/Video.jsx
+++ src/Components/Home/Video.jsx
@@ -1,16 +1,37 @@
+// import React from "react";
+
+// const Video = () => {
+//   return (
+//     <div className="h-full w-full">
+//       <video
+//         className="h-full w-full object-cover"
+//         autoPlay
+//         loop
+//         muted
+//         src="/video.mp4"
+//       ></video>
+//     </div>
+//   );
+// };
+
+// export default Video;
 import React from "react";
 
 const Video = () => {
   return (
-    <div className="h-full w-full">
-      <video
-        className="h-full w-full object-cover"
-        autoPlay
-        loop
-        muted
-        src="/video.mp4"
-      ></video>
-    </div>
+    <video
+      className="h-full w-full object-cover"
+      autoPlay
+      loop
+      muted
+      playsInline
+    >
+      <source
+        src="public\videos\video.mp4"
+        type="video/mp4"
+      />
+      Your browser does not support the video tag.
+    </video>
   );
 };
 
